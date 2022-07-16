59212a19ea48f40aeda5264c7015f5aa8bb402f8
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-07-16 21:13:01 +0200

    Updated package json

--- src/Components/AppProvider/AppProvider.tsx
+++ src/Components/AppProvider/AppProvider.tsx
@@ -2,14 +2,15 @@ import type { FC, PropsWithChildren } from "react";
 import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-
+import useSystemTheme from "react-use-system-theme";
 import themes from "../../Themes/themes.json";
 
 const AppProvider: FC<PropsWithChildren> = ({ children }): JSX.Element => {
   const queryClient = new QueryClient();
+  const systemTheme: keyof typeof themes = useSystemTheme("dark");
 
   return (
-    <ThemeProvider theme={themes["dark"]}>
+    <ThemeProvider theme={themes[systemTheme]}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>{children}</BrowserRouter>
       </QueryClientProvider>
