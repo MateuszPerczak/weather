68cd80a9a5e786124263b6f893df432f99bff4df
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2022-10-16 22:13:54 +0200

    Added Weather

---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+
+
+// const { isLoading, error, data } = useQuery<ApiResponse>([city], async () => {
+//     const response = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6967cc85728d1a5a59fa0fdd6086595e`
+//     );
+//     return await response.json();
+//   });
+
+
+
+const useWeather = () => {
+    
+};
